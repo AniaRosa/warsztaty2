b8a0bd77d842dbc877e2fb4c97f2bdff435879ea
Anna <anrosa.j@gmail.com>
2019-12-19 16:25:29 +0100

    warsztaty2 zadanie 4

---
+++ src/pl/coderslab/warsztaty2/models/Exercise.java
@@ -0,0 +1,40 @@
+package pl.coderslab.warsztaty2.models;
+
+public class Exercise {
+
+    private int id;
+    private String title;
+    private String description;
+
+    public Exercise(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Exercise(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
